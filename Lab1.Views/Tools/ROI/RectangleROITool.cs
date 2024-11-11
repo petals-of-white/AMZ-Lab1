e4864aad829d6ab979cbca50aaf1f1758fe716e9
Maksym Syvash <maxim.sivash@gmail.com>
2024-11-11 23:49:25 +0200

    work in progress

--- Lab1.Views/Tools/ROI/RectangleROITool.cs
+++ Lab1.Views/Tools/ROI/RectangleROITool.cs
@@ -9,7 +9,20 @@ public class RectangleROITool : ROITool
     public override bool IsDisplayed => Tool.IsDisplayed;
     public override PrimitiveType PrimitiveType => PrimitiveType.LineLoop;
     public RectangleROI Tool { get; set; } = new();
+    public float LineThickness { get; } = 0.01f;
 
+    public static (PointF left, PointF right) FromCenter(PointF center, float distance)
+    {
+        var left = center - new SizeF(distance / 2, 0);
+        var right = center + new SizeF(distance / 2, 0);
+
+        return (left, right);
+    }
+    public static PointF[] PrimitivesFromRectangle (Models.Shapes.Rectangle rectangle)
+    {
+        (var p1, var p2) = (rectangle.P1, rectangle.P2);
+
+    }
     protected override PointF [] Contour =>
         Tool.Region is Models.Shapes.Rectangle
         {
